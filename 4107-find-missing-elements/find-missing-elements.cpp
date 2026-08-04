3b12e57c9cbe617f67d91d7677969543203e488a
Dushyant sharma <Dushyant.000779@gmail.com>
2026-08-04 09:12:32 +0530

    Time: 0 ms (100.00%) | Memory: 32.2 MB (75.54%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-        vector<int>list={};
-       // unordered_map<int,int>mpp;
+        /*vector<int>list={};
         sort(nums.begin(),nums.end());
         int i=0;
         int j=nums.size()-1;
@@ -19,6 +18,22 @@ public:
         }
         }
         
-        return list;    
+        return list; */  // this is the better apprah as too many variables are there
+        // this is the optimal approach
+        vector<int> ans;
+        if (nums.empty()) return ans;
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size() - 1; i++) {
+            int curr = nums[i];
+
+            while (curr + 1 < nums[i + 1]) {
+                ans.push_back(curr + 1);
+                curr++;
+            }
+        }
+
+        return ans;   
     }
 };

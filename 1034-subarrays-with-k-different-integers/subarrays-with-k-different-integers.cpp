9601439438909be68eba38903480d30dd153758a
Dushyant sharma <Dushyant.000779@gmail.com>
2026-08-31 23:47:28 +0530

    Time: 79 ms (70.14%) | Memory: 56.3 MB (18.40%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int atMost(vector<int>&nums,int k){
+    int goodArray(vector<int>&nums,int k){
        int l=0,r=0,cnt=0;
         unordered_map<int,int>mpp;
         while(r<nums.size()){
@@ -27,6 +27,6 @@ public:
         }
         return cnt;*/
         
-        return atMost(nums,k)-atMost(nums,k-1);
+        return goodArray(nums,k)-goodArray(nums,k-1);
     }
 };
